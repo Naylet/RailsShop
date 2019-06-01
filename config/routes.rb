1ae32b65a49e31e49dfcbf79a57ed5d7e992a075
Natalia <nayleeeet@gmail.com>
2019-06-01 21:26:11 +0200

    Add users using device

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
-  #root 'welcome#index'
+  get 'store/index'
+  devise_for :users, controllers: {
+      registrations: 'registrations' ,
+      # confirmations: 'confirmations'
+  }
+
+  devise_scope :user do
+    get 'sign_up', to: 'devise/sessions#create'
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
+
+
+  root to: 'store#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
