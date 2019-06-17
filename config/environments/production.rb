9d2daebc928ff79bed4f455720980bb19f411d7d
Natalia <nayleeeet@gmail.com>
2019-06-18 01:39:33 +0200

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,20 +93,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST']}
-  config.action_mailer.default :charset => "utf-8"
 
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: ENV['MAIL_HOST'],
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      address: 'smtp.sendgrid.net',
+      port: "25",
+      domain: 'heroku.com',
+      user_name: ENV["SENDGRID_USERNAME"],
+      password: ENV["SENDGRID_PASSWORD"],
+      authentication: 'plain',
+      enable_starttls_auto: true
   }
+
 end
