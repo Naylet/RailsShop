e8e24f90b7f58d1e82d80cb4da1be96afc14517c
Natalia <nayleeeet@gmail.com>
2019-06-18 14:29:50 +0200

    Merge

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,5 @@
-<<<<<<< HEAD
 CarrierWave.configure do |config|
   config.ignore_integrity_errors = false
   config.ignore_processing_errors = false
   config.ignore_download_errors = false
-=======
-require ‘carrierwave/storage/abstract’
-require ‘carrierwave/storage/file’
-require ‘carrierwave/storage/fog’
-
-CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        use_iam_profile: true,
-        region: 'eu-west-2'
-    }
-    config.fog_directory = ENV['AWS_BUCKET']
-    config.fog_public = false
-    config.storage = :fog
-  else
-    config.storage = :file
-    config.enable_processing = Rails.env.development?
-  end
->>>>>>> 14f38be7b8f2badbe065eaaaeb9c1288ad93b322
 end
